4d27b947ebad573347d9425aca838f47a6ac8b35
Zytronium <dastelljes@outlook.com>
2026-04-18 16:29:38 -0500

    Fix bug where sometimes default number of rounds is 5 and sometimes 3 (should always default to 5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
 
     const resetGame = () => {
         setGameState('setup');
-        setNumRounds(3);
+        setNumRounds(5);
         setNumPlayers(2);
         setPlayerNames(['Player 1', 'Player 2']);
         setPlayers([]);
